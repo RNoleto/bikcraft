4c995be10b13b9f8b89cdba1a98c5221ba93791e
Ramon Ribeiro Noleto <95661913249@itajai.local>
2024-04-04 09:12:03 -0300

    ajuste de funções no header e footer

--- footer.php
+++ footer.php
@@ -11,7 +11,7 @@
 
 					<div class="grid-8 footer_historia">
 						<h3>Nossa História</h3>
-						<p>Quando iniciamos a Bikcraft queriamos apenas um produto que adoraríamos utilizar. Eramos apaixonados por pedalar e também por fazer as coisas com as nossas próprias mãos. Assim surgiu um sonho na garagem da nossa casa.</p>
+						<p>Quando iniciamos a <?php bloginfo('name'); ?> queriamos apenas um produto que adoraríamos utilizar. Eramos apaixonados por pedalar e também por fazer as coisas com as nossas próprias mãos. Assim surgiu um sonho na garagem da nossa casa.</p>
 					</div>
 
 					<div class="grid-4 footer_contato">
@@ -26,9 +26,9 @@
 					<div class="grid-4 footer_redes">
 						<h3>Contato</h3>
 						<ul>
-							<li><a href="http://facebook.com" target="_blank"><img src="img/redes-sociais/facebook.png" alt="Facebook Bikcraft"></a></li>
-							<li><a href="http://instagram.com" target="_blank"><img src="img/redes-sociais/instagram.png" alt="Instagram Bikcraft"></a></li>
-							<li><a href="http://twitter.com" target="_blank"><img src="img/redes-sociais/twitter.png" alt="Twitter Bikcraft"></a></li>
+							<li><a href="http://facebook.com" target="_blank"><img src="img/redes-sociais/facebook.png" alt="Facebook <?php bloginfo('name'); ?>"></a></li>
+							<li><a href="http://instagram.com" target="_blank"><img src="img/redes-sociais/instagram.png" alt="Instagram <?php bloginfo('name'); ?>"></a></li>
+							<li><a href="http://twitter.com" target="_blank"><img src="img/redes-sociais/twitter.png" alt="Twitter <?php bloginfo('name'); ?>"></a></li>
 						</ul>
 					</div>
 
@@ -37,7 +37,7 @@
 
 			<div class="copy">
 				<div class="container">
-					<p class="grid-16">Bikcraft 2015 - Alguns direitos reservados.</p>
+					<p class="grid-16"><?php bloginfo('name'); ?> <?php echo date("Y")?> - Alguns direitos reservados.</p>
 				</div>
 			</div>
 		</footer>
